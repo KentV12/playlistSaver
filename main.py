91bf27a87c98a004fa8d73fb7a44dac0eb6400a1
kent <kentvan21@gmail.com>
2023-05-26 18:41:24 -0500

    fix script not saving more than 50 videos in a playlist

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from datetime import date
 
-# Can only retrieve 50 playlists with 50 videos per playlist
 # To use application:
 #   - must add email address for testing in Google developer console
 #   - requires client_screts.json - downloaded from Google OAuth, this should be kept secret
+#   - token.pickle may need to be deleted if this script was used a long time ago
+
 
 def main():
     credentials = None
@@ -21,18 +22,20 @@ def main():
         print("Loading Credentials From File...")
         with open("token.pickle", "rb") as token:
             credentials = pickle.load(token)
-    
+
     # no credentials or invalid
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             print("Refreshing Access Token...")
-            credentials.refresh(Request()) # refresh invalid token
+            credentials.refresh(Request())  # refresh invalid token
         else:
             print("Fetching New Tokens...")
             scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-            flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", scopes) # loading credentials
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "client_secrets.json", scopes)  # loading credentials
 
-            flow.run_local_server(port=8080, promt="consent") # ask to consent with built in server host
+            # ask to consent with built in server host
+            flow.run_local_server(port=8080, promt="consent")
             credentials = flow.credentials
 
             # save the credentials for the next run
@@ -41,7 +44,8 @@ def main():
                 pickle.dump(credentials, f)
 
     youtube = build("youtube", "v3", credentials=credentials)
-    request = youtube.playlists().list(part="snippet,contentDetails", mine=True, maxResults=50) # maxResult default is 5, can be 0 to 50 (per page)
+    request = youtube.playlists().list(part="snippet,contentDetails", mine=True,
+                                       maxResults=50)  # maxResult default is 5, can be 0 to 50 (per page)
     response = request.execute()
 
     # study / troubleshoot purposes
@@ -49,32 +53,43 @@ def main():
     # print(response)
 
     # writing a text file
-    file = open(str(date.today()) + " Videos.txt", "w", errors="ignore") # some characters that cannot be read will return as errors
-    
+    # some characters that cannot be read will return as errors
+    file = open(str(date.today()) + " Videos.txt", "w", errors="ignore")
+
     # for each playlist, retrieve title
     for item in response["items"]:
         file.write("******************\n")
         file.write("Playlist: " + item["snippet"]["title"] + "\n")
-
-        # find videos from playlistId
-        videoRequest = youtube.playlistItems().list(part="snippet,contentDetails", playlistId=item["id"], maxResults=100) # maxResult default is 5, can be 0 to 50 (per page)
-        videoResponse = videoRequest.execute()
-
-        # study / troubleshoot purposes
-        # print("VIDEORESPONSE")
-        # print(videoResponse)
-
-        # write each video's title
+        
         videoCount = 1
-        for video in videoResponse["items"]:
-            file.write(str(videoCount) + ": " + video["snippet"]["title"] + "\n")
-            videoCount += 1
+        nextToken = None # playlist can only send a max of 50 videos. Next videos will be stored in the next token for request
+
+        while True:
+            # find videos from playlistId
+            videoRequest = youtube.playlistItems().list(part="snippet,contentDetails", playlistId=item["id"], maxResults=50, pageToken=nextToken)  # maxResult default is 5, can be 0 to 50 (per page)
+            videoResponse = videoRequest.execute()
+
+            nextToken = videoResponse.get("nextPageToken")
+
+            # study / troubleshoot purposes
+            # print("VIDEORESPONSE")
+            # print(videoResponse)
+
+            # write each video's title
+            for video in videoResponse["items"]:
+                file.write(str(videoCount) + ": " +
+                           video["snippet"]["title"] + "\n")
+                videoCount += 1
+
+            if not nextToken:
+                break
 
         file.write("\n")
 
     # close files and objects
     youtube.close()
     file.close()
+
 
 if __name__ == "__main__":
     main()
